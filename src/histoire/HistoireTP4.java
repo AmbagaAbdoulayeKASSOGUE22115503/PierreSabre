6a917754decf98bd3c4bf1dc57f2bad31111675d
AmbagaAbdoulayeKASSOGUE22115503 <ambaga.kass@gmail.com>
2023-11-14 08:15:17 +0100

    [200~“les humains ont des

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,5 +1,8 @@
 package histoire;
-import personnages.*;
+
+import personnages.Commerçant;
+import personnages.Ronin;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 
@@ -10,23 +13,38 @@ public class HistoireTP4 {
 //		humain.boire();
 //		humain.acheter("un jeu", 2);
 //		humain.acheter("un kimono", 50);
-		
-		Commerçant marco= new Commerçant("Marco",20);
+
+		// Commerçant marco= new Commerçant("Marco",20);
 //		marco.direBonjour();
 //		marco.seFaireExtorquer();
 //		marco.recevoir(15);
 //		marco.boire();
-		
-		Yakuza yaku= new Yakuza("Yaku Le Noir","whisky",45,"Warsong");
+
+		// Yakuza yaku= new Yakuza("Yaku Le Noir","whisky",45,"Warsong");
 //		yaku.direBonjour();
 //		yaku.extorquer(marco);
 //		System.out.println("");
-		
-		Ronin roro= new Ronin("Roro","shochu",54);
+
+		// Ronin roro= new Ronin("Roro","shochu",54);
 //		roro.direBonjour();
 //		roro.donner(marco);
-		
-		roro.provoquer(yaku);
+
+		// roro.provoquer(yaku);
+
+		Commerçant marco = new Commerçant("Marco", 20);
+		Commerçant chonin = new Commerçant("Chonin", 40);
+		Commerçant kumi = new Commerçant("Kumi", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+
+		marco.faireConnaissanceAvec(roro);
+		marco.faireConnaissanceAvec(yaku);
+		marco.faireConnaissanceAvec(chonin);
+		marco.faireConnaissanceAvec(kumi);
+
+		marco.listerConnaissance();
+		roro.listerConnaissance();
+		yaku.listerConnaissance();
 
 	}
 
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,59 +4,88 @@ public class Humain {
 	private String nom;
 	private String boissonPreferee;
 	private int quantiteArgent;
-	
-	
+	private int indiceMemoire = 0;
+	private int tailleMaxMemoire = 3;
+	private Humain[] memoire = new Humain[tailleMaxMemoire];
+
 	public Humain(String nom, String boissonPreferee, int quantiteArgent) {
 		this.nom = nom;
 		this.boissonPreferee = boissonPreferee;
 		this.quantiteArgent = quantiteArgent;
 	}
 
-
 	public String getNom() {
 		return nom;
 	}
 
-
 	public int getQuantiteArgent() {
 		return quantiteArgent;
 	}
-	
+
 	public void setQuantiteArgent(int quantiteArgent) {
 		this.quantiteArgent = quantiteArgent;
 	}
-	
-	public void direBonjour() {
-		String texte="Bonjour ! Je m’appelle "+nom+" et j’aime boire du "+boissonPreferee+".";
-		parler(texte);
-		
-	}
 
+	public void direBonjour() {
+		String texte = "Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boissonPreferee + ".";
+		parler(texte);
+
+	}
 
 	protected void parler(String texte) {
-		System.out.println("("+nom+") -"+texte);
-		
+		System.out.println("(" + nom + ") -" + texte);
+
 	}
-	
+
 	public void boire() {
-		parler("Mmmm, un bon verre de "+boissonPreferee+" ! GLOUPS !");
-		
+		parler("Mmmm, un bon verre de " + boissonPreferee + " ! GLOUPS !");
+
 	}
-	
-	public void  acheter(String bien, int prix){
-		if(quantiteArgent>=prix) {
-			parler("J'ai "+quantiteArgent+" sous en poche. Je vais pouvoir m'offrir "+bien+" à "+prix+" sous.");
-			quantiteArgent-=prix;
-		}
-		else
-			parler("Je n'ai plus que "+quantiteArgent+" en poche. Je ne peux pas m'offrir "+bien+" à "+prix+" sous.");
+
+	public void acheter(String bien, int prix) {
+		if (quantiteArgent >= prix) {
+			parler("J'ai " + quantiteArgent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix
+					+ " sous.");
+			quantiteArgent -= prix;
+		} else
+			parler("Je n'ai plus que " + quantiteArgent + " en poche. Je ne peux pas m'offrir " + bien + " à " + prix
+					+ " sous.");
 	}
-	
-	protected void gagnerArgent(int gain){
-		quantiteArgent+=gain;
+
+	protected void gagnerArgent(int gain) {
+		quantiteArgent += gain;
 	}
-	
-	protected void perdreArgent(int perte){
-		quantiteArgent-=perte;	
+
+	protected void perdreArgent(int perte) {
+		quantiteArgent -= perte;
 	}
+
+	public void ajouterMemoire(Humain humain) {
+		if (indiceMemoire >= tailleMaxMemoire) {
+			for (int i = 0; i < tailleMaxMemoire - 1; ++i)
+				memoire[i] = memoire[i + 1];
+
+			memoire[indiceMemoire - 1] = humain;
+		} else
+			memoire[indiceMemoire++] = humain;
+
+	}
+
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		this.direBonjour();
+
+		autreHumain.direBonjour();
+		autreHumain.ajouterMemoire(this);
+
+		this.ajouterMemoire(autreHumain);
+
+	}
+
+	public void listerConnaissance() {
+		String texte = "Je connais beaucoup de monde dont : ";
+		for (int i = 0; i < indiceMemoire; ++i)
+			texte += this.memoire[i].getNom() + ", ";
+		this.parler(texte.substring(0, texte.length() - 2));
+	}
+
 }
